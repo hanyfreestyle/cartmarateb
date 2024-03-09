cc1df4cc8a3e2ac7bbe2c5ba2059107d521714a4
Hany Darwish <hany.freestyle4u@gmail.com>
2024-03-09 21:53:15 +0200

    update

--- public/db/pro_product_photo_thumbnail.sql
+++ public/db/pro_product_photo_thumbnail.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2024 at 08:51 PM
+-- Generation Time: Mar 09, 2024 at 08:52 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
